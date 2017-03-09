ccc993606f9a07409d5607f501ad0f04b58cf21f
Ben Gibson <bgibson@edmunds.com>
2017-03-09 13:48:15 -0800

    Day 1 - excercise #2

--- subjects/Forms/exercise.js
+++ subjects/Forms/exercise.js
@@ -20,6 +20,39 @@ import ReactDOM from 'react-dom'
 import serializeForm from 'form-serialize'
 
 class CheckoutForm extends React.Component {
+  state = {
+    shippingName: "",
+    shippingState: "",
+    previous: {
+      shippingName: "",
+      shippingState: "",
+    },
+  }
+
+  updateShippingValue = (evt) => {
+    var newState = this.state;
+    newState[evt.target.name] = newState.previous[evt.target.name] = evt.target.value;
+    this.setState(newState);
+  }
+
+  setShippingSameAsBilling = (evt) => {
+    if (evt.target.checked) {
+      //set the shipping the same as the billing
+      evt.target.form.shippingName.readOnly = evt.target.form.shippingState.readOnly = true;
+      this.setState({
+        shippingName: evt.target.form.billingName.value,
+        shippingState: evt.target.form.billingState.value,
+      });
+    } else {
+      //set the shipping back to the previous value, before clicking the 'same as billing' button
+      evt.target.form.shippingName.readOnly = evt.target.form.shippingState.readOnly = false;
+      this.setState({
+        shippingName: this.state.previous.shippingName,
+        shippingState: this.state.previous.shippingState,
+      });
+    }
+  }
+
   render() {
     return (
       <div>
@@ -28,23 +61,30 @@ class CheckoutForm extends React.Component {
           <fieldset>
             <legend>Billing Address</legend>
             <p>
-              <label>Billing Name: <input type="text"/></label>
+              <label>Billing Name: <input type="text" name="billingName"/></label>
             </p>
             <p>
-              <label>Billing State: <input type="text" size="2"/></label>
+              <label>Billing State: <input type="text" size="2" name="billingState"/></label>
             </p>
           </fieldset>
 
           <br/>
 
           <fieldset>
-            <label><input type="checkbox"/> Same as billing</label>
+            <label><input type="checkbox" onChange={this.setShippingSameAsBilling}/> Same as billing</label>
             <legend>Shipping Address</legend>
             <p>
-              <label>Shipping Name: <input type="text"/></label>
+              <label>Shipping Name: <input type="text"
+                                           value={this.state.shippingName}
+                                           name="shippingName"
+                                           onChange={this.updateShippingValue}/></label>
             </p>
             <p>
-              <label>Shipping State: <input type="text" size="2"/></label>
+              <label>Shipping State: <input type="text"
+                                            value={this.state.shippingState}
+                                            size="2"
+                                            name="shippingState"
+                                            onChange={this.updateShippingValue}/></label>
             </p>
           </fieldset>
 
